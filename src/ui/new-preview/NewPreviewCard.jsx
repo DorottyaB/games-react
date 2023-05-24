08a32806e435fd50f8d9e583b918931adbe0317b
Dorottya <dorottyabalogh89@gmail.com>
2023-05-24 12:04:20 +0200

    add default image for games without any

--- src/ui/new-preview/NewPreviewCard.jsx
+++ src/ui/new-preview/NewPreviewCard.jsx
@@ -2,12 +2,19 @@
 import { Link } from 'react-router-dom';
 import { Rating } from '../../shared/rating/Rating';
 import { PlatformList } from '../../shared/platform-list/PlatformList';
+import noImage from '../../assets/no-image.png';
 import '../styles.css';
 
 export const NewPreviewCard = ({ game }) => {
   return (
     <Link to={`/games/${game.slug}`} className='preview-card-container'>
-      <img loading='lazy' src={game.background_image} width='355' height='200' alt='' />
+      <img
+        loading='lazy'
+        src={game.background_image !== null ? game.background_image : noImage}
+        width='355'
+        height='200'
+        alt=''
+      />
       <div className='info-container'>
         <h3>{game.name}</h3>
         <Rating rating={game.metacritic} />
--- src/ui/upcoming-preview/UpcomingCard.jsx
+++ src/ui/upcoming-preview/UpcomingCard.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import { formatDate } from '../../utils/Utils';
+import noImage from '../../assets/no-image.png';
 import '../styles.css';
 
 export const UpcomingCard = ({ game }) => {
   return (
     <div className='upcoming-card-container'>
       <div className='card-img-container'>
-        <img loading='lazy' src={game.background_image} width='270' height='152' alt='' />
+        <img
+          loading='lazy'
+          src={game.background_image !== null ? game.background_image : noImage}
+          width='270'
+          height='152'
+          alt=''
+        />
       </div>
       <Link to={`/games/${game.slug}`} className='link-title'>
         {game.name}
